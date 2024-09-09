4db324562f99a9bc7ea953b5402ee2ddf935a099
Perfect12356 <perfectitplacement@gmail.com>
2024-09-09 16:39:09 +0530

    new project

--- src/test/java/dataTable/StepDefination.java
+++ src/test/java/dataTable/StepDefination.java
@@ -4,23 +4,25 @@ import java.util.List;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import cucumber.annotation.en.Given;
-import cucumber.annotation.en.When;
-import cucumber.table.DataTable;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+//import io.cucumber.java.en;
 
 
 public class StepDefination {
 	WebDriver driver=null;
-	@Given("^the user on the login page$")
+	@Given("the user on the login page")
 	public void goToFacebook() {
-		driver =new ChromeDriver();
-		driver.get("https://facebook.com");
+		//driver =new ChromeDriver();
+	//driver.get("https://facebook.com");
+		System.out.println("Hello");
 		
 	}
-	@When("^user Enter Valid username password$")
-	public void EnterData(DataTable table)
+	@When("user Enter Valid username password")
+	public void EnterData()
 	{
-		List<List<String>> data=table.raw();
-		System.out.println(data.get(1).get(1));
+		//List<List<String>> data=table.raw();
+		//System.out.println(data.get(1).get(1));
+		System.out.println("Hello");
 	}
 }
--- target/test-classes/dataTable/dataTable.feature
+++ target/test-classes/dataTable/dataTable.feature
@@ -1,10 +1,10 @@
 Feature: Data Table
 Verify the Login Functionality
 	Scenario:
-	Given the user on the login page
-	When user Enter Valid username password
-	| Fields  | Values|
-	|username |akash@gmail.con|
-	|password |pass@123|
-	Then user loged in successfully
+		Given the user on the login page
+		When user Enter Valid username password
+		| Fields  | Values|
+		|username |akash@gmail.con|
+		|password |pass@123|
+		Then user loged in successfully
 	
